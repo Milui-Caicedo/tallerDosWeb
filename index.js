2f15f090516236b130533cfcb3352c258b05ab4a
Milui-Caicedo <luisa94_11@hotmail.com>
2018-10-28 18:23:18 -0600

    pagina de productos agregada y con estilos

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.set('view engine', 'handlebars');
 app.get('/', (reques, response) => {
   const collection = db.collection('fpproductos').find();
 
-  collection.toArray((err, docs)=>{
+  collection.toArray((err, docs) => {
 
     var contexto = {
       productos: docs,
@@ -38,13 +38,19 @@ app.get('/', (reques, response) => {
   });
 });
 
-app.get('/producto/:id',(reques,response)=>{
+app.get('/producto/:id', (reques, response) => {
   console.log(reques.params.id);
   const collection = db.collection('fpproductos').find({
     name: reques.params.id
   });
-  collection.toArray((err,res)=>{
-    response.send(res);
+  collection.toArray((err, res) => {
+    response.render('producto', {
+      name: res[0].name,
+      price : res[0].price,
+      age : res[0].age,
+      marca : res[0].marca,
+      carac : res[0].caracteristics
+    });
   });
 });
 
