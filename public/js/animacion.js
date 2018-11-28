d9e50fd63b3a62fd9fb6cef3c662cac2181056c1
milui <luisa94_11@hotmail.com>
2018-11-28 11:44:04 -0500

    primera parte animacion logo

---
+++ public/js/animacion.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',function(){
+
+    
+
+});
+
+function logo (s){
+    var logo = document.querySelector('#logo');
+    
+    //tween simple para el logo
+    TweenLite.from(logo, 1, {y:-50})
+};
