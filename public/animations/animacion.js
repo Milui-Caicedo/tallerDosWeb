0a17c47514bc0260f0484ed5d22147f3706acc88
milui <luisa94_11@hotmail.com>
2018-11-28 13:53:23 -0500

    esto aun no sirve aaaaaaa!

--- public/animations/animacion.js
+++ public/animations/animacion.js
@@ -1,12 +1,8 @@
 
-    //vamoas si funciona la transicion de los productos XD
-    /*var productos = document.querySelectorAll(".productos");
-    TweenLite.from(productos,2,{opacity:0, y:50});*/
-`
+    //vamos si funciona la transicion de los productos XD
+    var pro = document.getElementsByClassName("productos");
+    TweenLite.from(pro, 3 ,{opacity:0, y:50});
 
-function logo (s){
-    var logo = document.querySelector('#logo');
-    
-    //tween simple para el logo
-    TweenLite.from(logo, 1, {y:-50})
-};()`
+// animaion del logo
+    var logo = document.querySelector("#logo");
+    TweenLite.from(logo, 1, {y:-50});
