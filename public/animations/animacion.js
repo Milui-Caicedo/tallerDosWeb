53165786a729787ff67f76b85315106c54cf3060
milui <luisa94_11@hotmail.com>
2018-11-28 15:58:00 -0500

    ahora si funcionan los productos y el logo en el indexgit add .git add . yaygit add .!

--- public/animations/animacion.js
+++ public/animations/animacion.js
@@ -1,16 +1,19 @@
 //let tl = new TimelineLite();
-
-//vamos si funciona la transicion de los productos XD
     window.onload = function(){
-        var pro = document.querySelector(".productos");
-        TweenLite.from(pro, 2, {opacity:0, y:50});
-        //tl.add(TweenLite.to(pro, 3 ,{opacity:0, y:50}));
-        //console.log(pro);
-        //TweenLite.to(pro, 3 ,{opacity:0, y:50});
-    
-    // animaion del logo
+        var tl = new TimelineLite();
+
+        // animaion del logo
         var logo = document.querySelector(".img-landing");
+        tl.add(TweenLite.from(logo, 1 ,{opacity:0, y:-50}))
         //console.log(logo);
         //TweenLite.to(logo, 2, {bezier:[{left:100, top:50}, {left:100, top:0}, {left:200, top:150}], ease:Power1.easeInOut});
+        
+        //vamos si funciona la transicion de los productos XD
+        var pro = document.querySelector(".productos");
+        tl.add(TweenLite.from(pro, 1, {opacity:0, y:50}));
+        //tl.add(TweenLite.to(pro, 3 ,{opacity:0, y:50}));
+        //TweenLite.to(pro, 3 ,{opacity:0, y:50});
+    
+    
     }
     
