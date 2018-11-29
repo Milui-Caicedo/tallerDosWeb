fc866e795af4f101d10b3a59c82072926b793f7a
milui <luisa94_11@hotmail.com>
2018-11-29 07:53:08 -0500

    publicado

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
-{
+[
+    {
     
     name : "Rocky",
     price : 15000,
@@ -7,7 +8,7 @@
     descuento : true,
     categoria : "accesorios",
     caracteristics : "¡Rocki y Sparky, los nuevos amigos de aprendizaje interactivo de tu bebé, están listos para jugar y aprender! Este dúo interactivo interactúan uno con el otro, y con tu bebé, activando luces interactivas, música y frases que enseñan el abecedario, contar y más.  Tu bebé pasará horas de diversión con sus nuevos amigos, interactuando con ellos y explorando todas las actividades mientras estimula sus sentidos con luces brillantes y movimientos increíbles. ¡Chócalas! ¡Rocki graba lo que dices y lo repite usando tres voces graciosas! Además, con la tecnología de Aprendizaje por Etapas, ¡el contenido de aprendizaje cambia conforme tu bebé crece! Donde el desarrollo de tu pequeño se pone en movimiento. Enseñanza: Melodías increíbles y frases de aprendizaje enseñan el abecedario, colores, contar, figuras, inglés ¡y más! Habilidades sensoriales: Estimula los sentidos de tu bebé con las brillantes y coloridas luces, movimientos divertidos, música emocionante, y diferentes texturas. Curiosidad y asombro: Tu pequeño aprenderá causa y efecto mientras descubre cómo hacer que Rocki y Sparky se muevan con divertidas luces y sonidos."
-}
+},
 
 {
    
@@ -18,7 +19,7 @@
     descuento : false,
     categoria : "figuras",
     caracteristics : "¡Presentando a Adara Princesa Mágica! En los cielos de Zahramino, esta princesa genio usa polvos mágicos para hacer más poderosas las joyas y pociones. Esta linda muñeca Adara luce una falda de tul brillante, diadema verde y pulsera y zapatos que hacen juego. Usa el peine en forma de arcoíris para arreglarle su colita azul multicolor. Muñeca Adara Princesa Mágica de aproximadamente 15 cm"
-}
+},
 
 {
     name : "shimmer and shine",
@@ -28,7 +29,7 @@
     descuento : true,
     categoria : "figuras",
     caracteristics : "Haz que los deseos de tu pequeña genio cobren vida con un pack de juego de Teenie Genies con Shimmer y Zahrora, su Zahracornio. ¡Zahrora tiene alas brillantes que revolotean y agitan cuando la mueves! ¡Tu pequeño puede tener más aventuras añadiendo el Parque de Zahracornios Shimmer & Shine de Fisher-Price! Con montones de características divertidas (incluyendo un carrusel mágico), ¡tu pequeño se divertirá haciendo que sus Zahracornios vuelen y planeen por los cielos de Zahramino! El set de juego y figuras adicionales se venden por separado y están sujetos a disponibilidad. "
-}
+},
 
 {
     name : "Boomerang",
@@ -38,7 +39,7 @@
     descuento : false,
     categoria : "carros",
     caracteristics : "El Power Wheels® Boomerang™ es el vehículo de ensueño de cualquier competidor, permitiendo a los niños ladearse y girar en increíbles cambios totalmente controlados. Este increíble montable avanza sobre diferentes terrenos y avanza a una velocidad máxima de 8 km/h en dirección hacia adelante. Para mayor seguridad cuenta con un asiento ajustable, cinturón de seguridad, lados laterales altos y frenos Power-Lock®."
-}
+},
 
 {
     name : "Dinno racer",
@@ -48,7 +49,7 @@
     descuento : true,
     categoria : "carros",
     caracteristics : "¡Con el vehículo Jurassic World Dino Racer de Power Wheels, tu pequeño aventurero vivirá emocionantes aventuras con Blue, el raptor! Tu pequeño conductor avanzará sobre superficies duras, césped y terreno irregular a una velocidad máxima de 9.6 km/h con seguro de velocidad controlado por los padres para principiantes. ¡Incluso podrá abrir y cerrar la boca de Blue para increíbles morididas! ¡A tu pequeño aventurero le encantará recrear emocionantes aventuras todoterreno de la película tras el volante de este vehículo montable Power Wheels Jurassic World Dino Racer!"
-}
+},
 
 {
     "_id" : ObjectId("5bd5f6aa1138a160f9d79130"),
@@ -59,7 +60,7 @@
     descuento : false,
     categoria : "casas",
     caracteristics : "¡Mécete hacia divertidas aventuras con Tessa y Jack en la Little People® Compartiendo en la Casa del Árbol! Tessa y Jack son muy buenos amigos y les encanta jugar juntos. Al presionar el botón de descubrimiento, Tessa puede mecerse de atrás hacia adelante, mientras divertidas canciones cantadas y frases le enseñan a los pequeños sobre lo que es compartir y tomar turnos Mientras Jack espera su turno en el columpio, podrá jugar en la casa del árbol, y salir a través de la puerta secreta. ¡Sííí! ¡Pero cuidado! ¡Su amigo perrito también quiere jugar! Gira el segundo botón de descubrimiento para ver cómo sale el perrito de la casita a decir hola. Los pequeños se divertirán muchísimo al tomar turnos Tessa y Jack en Little People® Compartiendo en la Casa del Árbol Donde el desarrollo de los niños se pone en movimiento.™ Habilidades para relacionarse: Cuando los pequeños interactúan en la casita del árbol, se les enseña a tomar turnos, compartir y pensar sobre los otros a través de canciones, letras y frases. Habilidades de pensamiento: Cuando los pequeños descubren cómo hacer que el columpio se mueva al presionar el botón de descubrimiento, ¡ellos aprenden sobre el concepto de causa y efecto!"
-}
+},
 
 {
     name : "emociones y sentimientos",
@@ -69,7 +70,7 @@
     descuento : true,
     categoria : "accesorios",
     caracteristics : "Con dos divertidas páginas para explorar, este juguete Little People® See 'n Say® Emociones y Sentimientos introduce a los pequeños a ocho diferentes servidores públicos en un lado, ¡y ocho distintas emociones con sus expresiones correspondientes en el otro! Jala la palanca para ver a la flecha girar una y otra vez y llegar a cada uno de los servidores con animadas canciones, sonidos y frases. Después da vuelta a la página y gira para descubrir todas las emociones. ¡También tiene una emocionante modalidad de cuestionario! Pequeñas acciones que hacen la gran diferencia. Emociones y auto-expresión: Los pequeños pueden explorar distintas emociones y sus expresiones con la página de sentimientos que los introduce a un amplio rango de sentimiento, como la tristeza, confianza, frustración y determinación. Motricidad fina: Las pequeñas manos se ejercitan en grande mientras sostienen y llevan la palanca hacia enfrente, fortaleciendo la destreza y la coordinación ojos-manos. Curiosidad y asombro: Mientras los pequeños descubren cómo activar las canciones, sonidos y frases jalando la palanca, notan que sus acciones pueden hacer que sucedan cosas divertidas—hey, that's cause & effect!"
-}
+},
 
 {
     name : "granja cuidado de animales",
@@ -79,7 +80,7 @@
     descuento : false,
     categoria : "casas",
     caracteristics : "El granjero Jed trabaja muy fuerte para cuidar de todos sus animales en la Granja Cuidado de Animales Little People® ¡y le encantará que tu pequeño granjero le ayude! A tu pequeño le encantará aprender sobre cómo cuidar de todos sus amigos animales, desde alimentar a las gallinas hasta arropar a un caballo con una suave manta y explorar todas las emocionantes aventuras de este lugar. Que comience la diversión al presionar el botón de descubrimiento, abrir de manera sorpresa las puertas y escuchar divertidas canciones y frases. O abre el pajar para dejar caer la diversión ¡y activar más sonidos y frases! Después, tu pequeño granjero puede presionar el botón de descubrimiento para \"hacer salir del cascarón\" a los pollitos, girar el depósito de grano para rotar el cultivo de maíz (¡pop, pop, pop!), ¡y mucho más! ¡Ding, ding, ding! Haz sonar la campana para hacerles saber a los animales que es hora de comer.¡Dales un poco de maíz a las gallinas! Este set de juego Little People® lleno de actividades es mucho más que una simple granja, ¡es el hogar para los nuevos amigos de tu pequeño! Donde el desarrollo de los niños se pone en movimiento. Habilidades sensoriales: La granja ofrece una gran variedad de estímulos sensoriales para tu pequeño: desde colores brillantes, canciones y sonidos ¡hasta una variedad de materiales que podrá explorar con sus pequeñas manos! Curiosidad y asombro: Tu pequeño descubrirá todas las maneras de interactuar con esta granja, como presionar el botón de descubrimiento para escuchar divertidos sonidos y ver cómo se abren las escaleras. Verá, así, que sus acciones hacen que sucedan cosas divertidas ¡enseñándole el concepto de causa y efecto! Emociones y autoexpresión: Mientras tu pequeño juega a alimentar y cuidar a los animales de la granja, aprende lo que se siente cuidar de otros."
-}
+},
 
 {
     name : "James",
@@ -89,8 +90,8 @@
     descuento : true,
     categoria : "figuras",
     caracteristics : "Los vehículos y sets de trenes de Thomas & Friends™ Railway Pals brindan juego lleno de diversión que estimula el desarrollo de habilidades de motricidad, habilidades de pensamiento, aprendizaje e imaginación de tu pequeño. Estas increíbles locomotoras interactúan con puntos de activación de vía en los sets de trenes Railway Pals (se venden por separado y están sujetos a disponibilidad) para brindar sonidos, música y frases que enseñan letras, contar y colores. Presiona la defensa de Thomas que se ilumina para escuchar cómo le enseña a tu pequeño la letra T, el color azul y a contar de 1 a 3. Tu pequeño también podrá rodarlo sobre puntos de activación en sets de trenes interactivos Railway Pals para más sonidos, canciones y frases educativas. Además, mientras tu pequeño agarra a Thomas, lo rueda y crea aventuras únicas, participa en juego que estimula habilidades de motricidad e imaginación. Colecciona todos los vehículos y sets de trenes interactivos Railway Pals para crear un mundo de juego educativo con trenes rodantes que hablan y cantan. (Se venden por separado y están sujetos a disponibilidad.)"
-}
-/
+},
+
 {
     name : "Tomas",
     price : 95000,
@@ -99,4 +100,5 @@
     descuento : false,
     categoria : "figuras",
     caracteristics : "Los vehículos y sets de trenes de Thomas & Friends™ Railway Pals brindan juego lleno de diversión que estimula el desarrollo de habilidades de motricidad, habilidades de pensamiento, aprendizaje e imaginación de tu pequeño. Estas increíbles locomotoras interactúan con puntos de activación de vía en los sets de trenes Railway Pals (se venden por separado y están sujetos a disponibilidad) para brindar sonidos, música y frases que enseñan letras, contar y colores. Presiona la defensa de Thomas que se ilumina para escuchar cómo le enseña a tu pequeño la letra T, el color azul y a contar de 1 a 3. Tu pequeño también podrá rodarlo sobre puntos de activación en sets de trenes interactivos Railway Pals para más sonidos, canciones y frases educativas. Además, mientras tu pequeño agarra a Thomas, lo rueda y crea aventuras únicas, participa en juego que estimula habilidades de motricidad e imaginación. Colecciona todos los vehículos y sets de trenes interactivos Railway Pals para crear un mundo de juego educativo con trenes rodantes que hablan y cantan. (Se venden por separado y están sujetos a disponibilidad.)"
-}
+},
+]
